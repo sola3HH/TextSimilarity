7dd32a2844a6140c6c91865741596160571a9048
sanhuanghuang <13551296510@163.com>
2020-08-06 13:55:43 +0800

    余弦相似模型添加遍历功能

--- similarity_cos.py
+++ similarity_cos.py
@@ -1,5 +1,5 @@
 import re
-
+import os
 import nltk
 from gensim import corpora
 
@@ -16,17 +16,21 @@ def cut_word(str):
     return nltk.word_tokenize(str)
 
 
+# 确认文件路径
+filePath = 'texts/'
 # 分词
-texts = [tokenization('test1.txt'), tokenization('标准答案.txt')]
+texts = []
+for fileName in os.listdir(filePath):
+    texts.append(tokenization('texts/' + fileName))
 # 建立词袋并向量化（语料库）
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2idx(text) for text in texts]
 
 # 构建原文本
-origin = tokenization('标准答案.txt')
+origin = tokenization('texts/' + '标准答案.txt')
 origin_bow = dictionary.doc2bow(origin)
 origin_vector = []
-
+# 构建原文本词频向量
 for i in range(0, len(dictionary)):
     flag = 0
     for index in origin_bow:
@@ -37,32 +41,35 @@ for i in range(0, len(dictionary)):
     if flag == 0:
         origin_vector.append(0)
 
-# 遍历构建构建query文本
-query = tokenization('test1.txt')
-query_bow = dictionary.doc2bow(query)
-query_vector = []
+# 遍历构建构建查询文本
+for query_text in os.listdir(filePath):
+    if query_text == '标准答案.txt':
+        continue
+    query = tokenization('texts/' + query_text)
+    query_bow = dictionary.doc2bow(query)
+    query_vector = []
+    # 构建查询文本词频向量
+    for i in range(0, len(dictionary)):
+        flag = 0
+        for index in query_bow:
+            if index[0] == i:
+                query_vector.append(index[1])
+                flag = 1
+                break
+        if flag == 0:
+            query_vector.append(0)
 
-for i in range(0, len(dictionary)):
-    flag = 0
-    for index in query_bow:
-        if index[0] == i:
-            query_vector.append(index[1])
-            flag = 1
-            break
-    if flag == 0:
-        query_vector.append(0)
+    # 余弦相似度
+    A, B, AB = 0, 0, 0
+    for value in origin_vector:
+        A += value ** 2
 
-# 余弦相似度
-A, B, AB = 0, 0, 0
-for value in origin_vector:
-    A += value ** 2
+    for value in query_vector:
+        B += value ** 2
 
-for value in query_vector:
-    B += value ** 2
+    for i in range(0, len(dictionary)):
+        AB += origin_vector[i] * query_vector[i]
 
-for i in range(0, len(dictionary)):
-    AB += origin_vector[i] * query_vector[i]
+    sim = AB / ((A ** 0.5) * (B ** 0.5))
 
-sim = AB / ((A ** 0.5) * (B ** 0.5))
-
-print('两篇文章的相似度为：%.2f' % (sim * 100) + "%")
+    print('文本' + query_text + '与原文的相似度为：%.2f' % (sim * 100) + "%")
