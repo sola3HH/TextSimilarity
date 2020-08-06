7c47432bdfd5addd47576ab1824698ed815ea6c9
sanhuanghuang <13551296510@163.com>
2020-08-06 12:35:12 +0800

    cos updated

--- similarity_cos.py
+++ similarity_cos.py
@@ -12,26 +12,57 @@ def tokenization(fileName):
         return nltk.word_tokenize(str)
 
 
+def cut_word(str):
+    return nltk.word_tokenize(str)
+
+
 # 分词
-texts = [tokenization('test1.txt'), tokenization('test2.txt')]
+texts = [tokenization('test1.txt'), tokenization('标准答案.txt')]
 # 建立词袋并向量化（语料库）
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2idx(text) for text in texts]
+
 # 构建原文本
-origin = tokenization('test1.txt')
-origin_vector = dictionary.doc2idx(origin)
-# 构建query文本
-query = tokenization('test2.txt')
-query_vector = dictionary.doc2idx(query)
+origin = tokenization('标准答案.txt')
+origin_bow = dictionary.doc2bow(origin)
+origin_vector = []
+
+for i in range(0, len(dictionary)):
+    flag = 0
+    for index in origin_bow:
+        if index[0] == i:
+            origin_vector.append(index[1])
+            flag = 1
+            break
+    if flag == 0:
+        origin_vector.append(0)
+
+# 遍历构建构建query文本
+query = tokenization('test1.txt')
+query_bow = dictionary.doc2bow(query)
+query_vector = []
+
+for i in range(0, len(dictionary)):
+    flag = 0
+    for index in query_bow:
+        if index[0] == i:
+            query_vector.append(index[1])
+            flag = 1
+            break
+    if flag == 0:
+        query_vector.append(0)
 
 # 余弦相似度
-A, B = 0, 0
+A, B, AB = 0, 0, 0
 for value in origin_vector:
     A += value ** 2
 
 for value in query_vector:
     B += value ** 2
 
-sim = A / ((A ** 0.5) * (B ** 0.5))
+for i in range(0, len(dictionary)):
+    AB += origin_vector[i] * query_vector[i]
+
+sim = AB / ((A ** 0.5) * (B ** 0.5))
 
 print('两篇文章的相似度为：%.2f' % (sim * 100) + "%")
